9b9374c902237e313bebf6a154c0e026a11102ce
JuanAmaya14 <iefaz.juanamaya@gmail.com>
2021-11-21 12:28:58 -0500

    intento 2 de combinar con tkinter

--- whatsapp.py
+++ whatsapp.py
@@ -1,10 +1,41 @@
-#importa selenium que es el encargado de hacer la automatizacion
+########################### TKINTER #########################
+
+import tkinter as tk
+from tkinter import ttk
+
+def enviar(name):
+    name = name.get()
+    ventana.quit
+    return name
+
+ventana = tk.Tk()
+ventana.title("Principal")
+ventana.geometry('380x300')
+ventana.configure(background='black')
+
+etiqueta1 = tk.Label(ventana,text = "Enviar mensaje", bg= "black", fg = "snow")
+name = ttk.Entry(ventana)
+aceptar = ttk.Button(ventana, text="Aceptar", command=enviar(name))
+cancelar = ttk.Button(ventana,text="Cancelar")
+
+
+name.place(x=50, y=50)
+
+aceptar.place(x=150, y=100)
+cancelar.place(x=60, y=100)
+
+etiqueta1.pack(fill = tk.X)
+
+ventana.mainloop() 
+
+###################### SELENIUM ############################
+
 
 from time import sleep
 import unittest
 from selenium import webdriver
 
-name = input('Nombre de contacto o grupo: ') #Pedir nombre del un grupo o contacto (no se puede con caracteres especiales como emojis)
+#name = input('Nombre de contacto o grupo: ') #Pedir nombre del un grupo o contacto (no se puede con caracteres especiales como emojis)
 
 #clase de pasos a seguir
 class SendMessage(unittest.TestCase):
